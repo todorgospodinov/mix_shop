1816fe870fc9598573fbeb5d6bf31bff63c9a2e1
todor gospodinov <togo@gbg.bg>
2021-02-25 14:14:04 +0200

    Initial commit

---
+++ src/main/java/softuni/mix_shop/MixShopApplication.java
@@ -0,0 +1,13 @@
+package softuni.mix_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MixShopApplication.class, args);
+    }
+
+}
